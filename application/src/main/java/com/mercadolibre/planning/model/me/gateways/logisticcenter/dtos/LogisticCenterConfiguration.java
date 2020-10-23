b4740820d08a03fd6e5e609500ae5728704f0028
candecp <cande.cp@gmail.com>
2020-10-23 16:48:29 -0300

    Handle null projected_end_date (#25)
    * Handle null projected_end_date
    
    * Some fixes in dates (#27)
    
    * Consider UTC timezone when getting headcount, tph and productivity
    
    * Avoid returning projection results with UTC dates
    
    * Add test

--- application/src/main/java/com/mercadolibre/planning/model/me/gateways/logisticcenter/dtos/LogisticCenterConfiguration.java
+++ application/src/main/java/com/mercadolibre/planning/model/me/gateways/logisticcenter/dtos/LogisticCenterConfiguration.java
@@ -2,7 +2,7 @@ package com.mercadolibre.planning.model.me.gateways.logisticcenter.dtos;
 
 import lombok.Value;
 
-import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.TimeZone;
 
 @Value
@@ -10,7 +10,7 @@ public class LogisticCenterConfiguration {
 
     private TimeZone timeZone;
 
-    public LocalDateTime getLocalDateTime() {
-        return LocalDateTime.now(getTimeZone().toZoneId());
+    public ZoneId getZoneId() {
+        return timeZone.toZoneId();
     }
 }
--- core/src/main/java/com/mercadolibre/planning/model/me/entities/projection/chart/ChartData.java
+++ core/src/main/java/com/mercadolibre/planning/model/me/entities/projection/chart/ChartData.java
@@ -4,27 +4,42 @@ import com.mercadolibre.planning.model.me.entities.projection.ProjectionResult;
 import lombok.Value;
 
 import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 
+import static com.mercadolibre.planning.model.me.utils.DateUtils.convertToTimeZone;
 import static java.time.format.DateTimeFormatter.ofPattern;
 
 @Value
 public class ChartData {
-    private String title;
-    private String cpt;
-    private String projectedEndTime;
 
     private static final DateTimeFormatter DATE_FORMATTER = ofPattern("yyyy-MM-dd'T'HH:mm:ssZ");
     private static final DateTimeFormatter HOUR_FORMAT = ofPattern("HH:mm");
 
+    private String title;
+    private String cpt;
+    private String projectedEndTime;
+
     public static ChartData fromProjectionResponse(final ProjectionResult projectionResult,
-                                                   final ZoneId zoneId) {
+                                                   final ZoneId zoneId,
+                                                   final ZonedDateTime utcDateTo) {
+        final ZonedDateTime cpt = convertToTimeZone(zoneId, projectionResult.getDate());
+        final ZonedDateTime dateTo = convertToTimeZone(zoneId, utcDateTo);
+        final ZonedDateTime projectedEndDate = convertToTimeZone(zoneId,
+                projectionResult.getProjectedEndDate());
+
         return new ChartData(
-                projectionResult.getDate().toLocalDateTime().atZone(zoneId)
-                        .format(HOUR_FORMAT.withZone(zoneId)),
-                projectionResult.getDate().toLocalDateTime().atZone(zoneId).format(DATE_FORMATTER),
-                projectionResult.getProjectedEndDate().toLocalDateTime().atZone(zoneId)
-                        .format(DATE_FORMATTER.withZone(zoneId))
+                cpt.format(HOUR_FORMAT),
+                cpt.format(DATE_FORMATTER),
+                getDefaultProjectedEndTime(projectedEndDate, dateTo)
         );
+    }
+
+    private static String getDefaultProjectedEndTime(final ZonedDateTime projectEndDate,
+                                                     final ZonedDateTime dateTo) {
+
+        return  projectEndDate == null
+                ? dateTo.format(DATE_FORMATTER)
+                : projectEndDate.format(DATE_FORMATTER);
     }
 }
