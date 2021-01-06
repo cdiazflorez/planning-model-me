61bdacacf007b347a1886d27c29eddffd18cb96c
Pablo Lucadei <pablonazareno@gmail.com>
2021-01-06 13:53:43 -0300

    fix kraken enviroment configuration

--- infrastructure/src/main/java/com/mercadolibre/planning/model/me/clients/groot/KrakenConfig.java
+++ infrastructure/src/main/java/com/mercadolibre/planning/model/me/clients/groot/KrakenConfig.java
@@ -2,12 +2,16 @@ package com.mercadolibre.planning.model.me.clients.groot;
 
 import com.mercadolibre.kraken.client.KrakenLibrary;
 import com.mercadolibre.kraken.client.config.KrakenClientConfiguration;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@Data
 @Configuration
 @ConfigurationProperties("kraken.client")
+@Slf4j
 public class KrakenConfig {
 
     private int connectionTimeout = 1000;
@@ -17,6 +21,7 @@ public class KrakenConfig {
 
     @Bean
     public KrakenClient getClient() {
+        log.info("Initializing Kraken library. scope {}", scope);
         KrakenClientConfiguration configuration = KrakenClientConfiguration.builder()
                 .withConnectionTimeout(connectionTimeout)
                 .withSocketTimeout(socketTimeout)
