15cd85c99434921805a5593c30d86503482167a1
Marcial Manzo <marcial.manzo@mercadolibre.com>
2021-08-20 12:18:17 -0300

    flow backlog api client (#289)
    * adds rest pool for backlog-api client
    
    * adds backlog-api rest client and tests
    
    * adds flow-backlog base url to config file

---
+++ infrastructure/src/main/java/com/mercadolibre/planning/model/me/clients/rest/backlog/BacklogApiClient.java
@@ -0,0 +1,63 @@
+package com.mercadolibre.planning.model.me.clients.rest.backlog;
+
+import com.mercadolibre.fbm.wms.outbound.commons.rest.HttpClient;
+import com.mercadolibre.fbm.wms.outbound.commons.rest.HttpRequest;
+import com.mercadolibre.json.type.TypeReference;
+import com.mercadolibre.planning.model.me.clients.rest.backlog.request.BacklogRequest;
+import com.mercadolibre.planning.model.me.clients.rest.backlog.response.Backlog;
+import com.mercadolibre.planning.model.me.clients.rest.config.RestPool;
+import com.mercadolibre.restclient.MeliRestClient;
+
+import java.time.ZonedDateTime;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import static java.lang.String.format;
+import static java.time.format.DateTimeFormatter.ISO_OFFSET_DATE_TIME;
+import static org.springframework.http.HttpStatus.OK;
+
+public class BacklogApiClient extends HttpClient {
+    private static final String BACKLOG_URL = "/flow/backlogs/logistic_centers/%s/backlogs";
+
+    public BacklogApiClient(final MeliRestClient client) {
+        super(client, RestPool.BACKLOG.name());
+    }
+
+    public List<Backlog> getBacklog(BacklogRequest request) {
+        final HttpRequest httpRequest = HttpRequest.builder()
+                .url(format(BACKLOG_URL, request.getWarehouseId()))
+                .GET()
+                .queryParams(getQueryParams(request))
+                .acceptedHttpStatuses(Set.of(OK))
+                .build();
+
+        return send(httpRequest, response ->
+                response.getData(new TypeReference<>() {})
+        );
+    }
+
+    private Map<String, String> getQueryParams(BacklogRequest request) {
+        Map<String, String> params = new HashMap<>();
+        addAsQueryParam(params, "workflows", request.getWorkflows());
+        addAsQueryParam(params, "processes", request.getProcesses());
+        addAsQueryParam(params, "group_by", request.getGroupingFields());
+        addAsQueryParam(params, "date_from", request.getDateFrom());
+        addAsQueryParam(params, "date_to", request.getDateTo());
+
+        return params;
+    }
+
+    private void addAsQueryParam(Map<String, String> map, String key, List<String> value) {
+        if (value != null) {
+            map.put(key, String.join(",", value));
+        }
+    }
+
+    private void addAsQueryParam(Map<String, String> map, String key, ZonedDateTime value) {
+        if (value != null) {
+            map.put(key, value.format(ISO_OFFSET_DATE_TIME));
+        }
+    }
+}
---
+++ infrastructure/src/main/java/com/mercadolibre/planning/model/me/clients/rest/backlog/response/Backlog.java
@@ -0,0 +1,19 @@
+package com.mercadolibre.planning.model.me.clients.rest.backlog.response;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.mercadolibre.planning.model.me.utils.CustomDateZoneDeserializer;
+import lombok.Value;
+
+import java.time.ZonedDateTime;
+import java.util.Map;
+
+@Value
+public class Backlog {
+
+    @JsonDeserialize(using = CustomDateZoneDeserializer.class)
+    private ZonedDateTime date;
+
+    private Map<String, String> keys;
+
+    private Integer total;
+}
