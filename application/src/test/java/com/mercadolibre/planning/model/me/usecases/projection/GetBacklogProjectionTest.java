3ef0a1f9f770ccb10f449e522b7092e92c6ae514
Marcial Manzo <marcial.manzo@mercadolibre.com>
2021-08-25 17:26:17 -0300

    splits backlog projection from view building (#291)

--- application/src/test/java/com/mercadolibre/planning/model/me/usecases/projection/GetBacklogProjectionTest.java
+++ application/src/test/java/com/mercadolibre/planning/model/me/usecases/projection/GetBacklogProjectionTest.java
@@ -17,6 +17,7 @@ import com.mercadolibre.planning.model.me.gateways.planningmodel.projection.back
 import com.mercadolibre.planning.model.me.gateways.planningmodel.projection.backlog.response.ProjectionValue;
 import com.mercadolibre.planning.model.me.usecases.monitor.dtos.monitordata.process.ProcessInfo;
 import com.mercadolibre.planning.model.me.usecases.projection.dtos.BacklogProjectionInput;
+import com.mercadolibre.planning.model.me.usecases.projection.entities.ProjectedBacklog;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -46,53 +47,27 @@ import static org.mockito.Mockito.when;
 @ExtendWith(MockitoExtension.class)
 public class GetBacklogProjectionTest {
 
-    @InjectMocks
-    private GetBacklogProjection getBacklogProjection;
-
     @Mock
     protected PlanningModelGateway planningModel;
-
     @Mock
     protected LogisticCenterGateway logisticCenter;
-
+    @InjectMocks
+    private GetBacklogProjection getBacklogProjection;
     @Mock
-    private BacklogGatewayProvider backlogGatewayProvider;
-
-    @Mock
-    private BacklogGateway backlogGateway;
+    private ProjectBacklog projectBacklog;
 
     @Test
     public void testExecute() {
         // GIVEN
-        when(backlogGatewayProvider.getBy(WORKFLOW)).thenReturn(Optional.of(backlogGateway));
-
-        when(backlogGateway.getBacklog(
-                List.of(
-                        Map.of("status", "pending"),
-                        Map.of("status", "to_pack")
-                ),
-                WAREHOUSE_ID,
-                A_DATE,
-                A_DATE.plusHours(25),
-                true)
-        ).thenReturn(
-                new ArrayList<>(
-                        List.of(
-                                ProcessBacklog.builder()
-                                        .process("to_pack")
-                                        .quantity(1442)
-                                        .build()
-                        ))
-        );
-
-        when(backlogGateway.getUnitBacklog(new UnitProcessBacklogInput("to_pick",
-                WAREHOUSE_ID, A_DATE, A_DATE.plusHours(25), null, ORDER_GROUP_TYPE, true))
-        ).thenReturn(
-                ProcessBacklog.builder()
-                        .process(ProcessInfo.PICKING.getStatus())
-                        .quantity(2232)
-                        .build()
-        );
+        final BacklogProjectionInput input = BacklogProjectionInput.builder()
+                .workflow(WORKFLOW)
+                .warehouseId(WAREHOUSE_ID)
+                .processName(List.of(WAVING, PICKING, PACKING))
+                .userId(1L)
+                .dateFrom(A_DATE)
+                .dateTo(A_DATE.plusHours(25))
+                .groupType(ORDER_GROUP_TYPE)
+                .build();
 
         final ZonedDateTime firstDate = getNextHour(A_DATE);
 
@@ -119,18 +94,8 @@ public class GetBacklogProjectionTest {
                         .build()
         ));
 
-        when(planningModel.getBacklogProjection(BacklogProjectionRequest.builder()
-                .warehouseId(WAREHOUSE_ID)
-                .workflow(WORKFLOW)
-                .processName(List.of(WAVING, PICKING, PACKING))
-                .dateFrom(A_DATE)
-                .dateTo(firstDate.plusHours(25))
-                .currentBacklog(List.of(
-                        new CurrentBacklog(WAVING, 0),
-                        new CurrentBacklog(PICKING, 2232),
-                        new CurrentBacklog(PACKING, 1442)))
-                .applyDeviation(true)
-                .build())).thenReturn(List.of(
+        when(projectBacklog.execute(input))
+                .thenReturn(new ProjectedBacklog(List.of(
                         new BacklogProjectionResponse(WAVING, List.of(
                                 new ProjectionValue(firstDate, 100),
                                 new ProjectionValue(firstDate.plusHours(1), 200)
@@ -142,21 +107,11 @@ public class GetBacklogProjectionTest {
                         new BacklogProjectionResponse(PACKING, List.of(
                                 new ProjectionValue(firstDate, 130),
                                 new ProjectionValue(firstDate.plusHours(1), 230)
-                        ))
-        ));
+                        )))
+                ));
 
         when(logisticCenter.getConfiguration(WAREHOUSE_ID)).thenReturn(
                 new LogisticCenterConfiguration(getDefault()));
-
-        final BacklogProjectionInput input = BacklogProjectionInput.builder()
-                .workflow(WORKFLOW)
-                .warehouseId(WAREHOUSE_ID)
-                .processName(List.of(WAVING, PICKING, PACKING))
-                .userId(1L)
-                .dateFrom(A_DATE)
-                .dateTo(A_DATE.plusHours(25))
-                .groupType(ORDER_GROUP_TYPE)
-                .build();
 
         // WHEN
         final BacklogProjection response = getBacklogProjection.execute(input);
