4fb87e50bb393f95c573ee48070e8ab171d2d357
martincucu <martincucu@users.noreply.github.com>
2021-09-29 09:00:17 -0300

    Fix/backlog monitor quantities (#327)

--- infrastructure/src/main/java/com/mercadolibre/planning/model/me/controller/GetBacklogProjectionRequest.java
+++ infrastructure/src/main/java/com/mercadolibre/planning/model/me/controller/GetBacklogProjectionRequest.java
@@ -43,6 +43,7 @@ public class GetBacklogProjectionRequest {
                 .dateTo(dateTo)
                 .userId(callerId)
                 .processName(processName)
+                .groupType("order")
                 .build();
     }
 }
