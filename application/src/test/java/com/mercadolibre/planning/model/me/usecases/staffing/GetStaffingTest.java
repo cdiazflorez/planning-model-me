c03064fff43afb5ebccee64f3c9a0eaa8a7fba9e
fpertuz <fabio.pertuz@mercadolibre.com.co>
2022-02-24 19:35:37 -0500

    :construction: [ME] FWCO-18: GetStaffingtest updated with Witdrawals

--- application/src/test/java/com/mercadolibre/planning/model/me/usecases/staffing/GetStaffingTest.java
+++ application/src/test/java/com/mercadolibre/planning/model/me/usecases/staffing/GetStaffingTest.java
@@ -35,6 +35,22 @@ import static org.mockito.Mockito.when;
 @ExtendWith(MockitoExtension.class)
 class GetStaffingTest {
 
+    private static String OUTBOUND_WORKFLOW = "fbm-wms-outbound";
+    private static String INBOUND_WORKFLOW = "fbm-wms-inbound";
+    private static String WITHDRAWALS_WORKFLOW = "fbm-wms-withdrawals";
+    private static String RECEIVING_PROCESS = "receiving";
+    private static String CHECK_IN_PROCESS = "check_in";
+    private static String PUT_AWAY_PROCESS = "put_away";
+    private static String PICKING_PROCESS = "picking";
+    private static String BATCH_SORTER_PROCESS = "batch_sorter";
+    private static String WALL_IN_PROCESS = "wall_in";
+    private static String PACKING_PROCESS = "packing";
+    private static String PACKING_WALL_PROCESS = "packing_wall";
+    private static String AREA_MZ1 = "MZ-1";
+    private static String AREA_RKL = "RK-L";
+
+
+
     @InjectMocks
     private GetStaffing useCase;
 
@@ -59,96 +75,69 @@ class GetStaffingTest {
         final Staffing staffing = useCase.execute(input);
 
         //THEN
-        assertEquals(176, staffing.getTotalWorkers());
-        assertEquals(3, staffing.getWorkflows().size());
-
         final StaffingWorkflow outbound = staffing.getWorkflows().stream()
-                .filter(w -> w.getWorkflow().equals("fbm-wms-outbound"))
+                .filter(w -> w.getWorkflow().equals(OUTBOUND_WORKFLOW))
                 .findFirst().orElseThrow();
 
         final StaffingWorkflow inbound = staffing.getWorkflows().stream()
-                .filter(w -> w.getWorkflow().equals("fbm-wms-inbound"))
+                .filter(w -> w.getWorkflow().equals(INBOUND_WORKFLOW))
                 .findFirst().orElseThrow();
 
         final StaffingWorkflow withdrawals = staffing.getWorkflows().stream()
-                .filter(w -> w.getWorkflow().equals("fbm-wms-withdrawals"))
+                .filter(w -> w.getWorkflow().equals(WITHDRAWALS_WORKFLOW))
                 .findFirst().orElseThrow();
 
-        assertEquals("fbm-wms-outbound", outbound.getWorkflow());
-        assertEquals(102, outbound.getTotalWorkers());
-        assertEquals(20, outbound.getTotalNonSystemicWorkers());
-        assertEquals(5, outbound.getProcesses().size());
-
+        final Process putAway = inbound.getProcesses().get(2);
         final Process picking = outbound.getProcesses().get(0);
-        assertEquals("picking", picking.getProcess());
-        assertEquals(45, picking.getNetProductivity());
-        assertEquals(30, picking.getTargetProductivity());
-        assertEquals(20, picking.getWorkers().getBusy());
-        assertEquals(10, picking.getWorkers().getIdle());
-        assertEquals(2700, picking.getThroughput());
-
-        final var pickingAreas = picking.getAreas();
-        assertEquals(2, pickingAreas.size());
-        assertEquals("MZ1", pickingAreas.get(0).getArea());
-        assertEquals(10, pickingAreas.get(0).getWorkers().getBusy());
-        assertEquals(0, pickingAreas.get(0).getWorkers().getIdle());
-        assertEquals(60, pickingAreas.get(0).getNetProductivity());
-        assertEquals("MZ2", pickingAreas.get(1).getArea());
-        assertEquals(10, pickingAreas.get(1).getWorkers().getBusy());
-        assertEquals(0, pickingAreas.get(1).getWorkers().getIdle());
-        assertEquals(75, pickingAreas.get(1).getNetProductivity());
-
-        final Process batchSorter = outbound.getProcesses().get(1);
-        assertEquals("batch_sorter", batchSorter.getProcess());
-        assertNull(batchSorter.getNetProductivity());
-        assertNull(batchSorter.getTargetProductivity());
-        assertNull(batchSorter.getWorkers().getBusy());
-        assertNull(batchSorter.getWorkers().getIdle());
-        assertNull(batchSorter.getThroughput());
-
-        final Process wallIn = outbound.getProcesses().get(2);
-        assertEquals("wall_in", wallIn.getProcess());
-        assertEquals(0, wallIn.getNetProductivity());
-        assertEquals(20, wallIn.getTargetProductivity());
-        assertEquals(0, wallIn.getWorkers().getBusy());
-        assertEquals(0, wallIn.getWorkers().getIdle());
-        assertEquals(0, wallIn.getThroughput());
-
-        final Process packing = outbound.getProcesses().get(3);
-        assertEquals("packing", packing.getProcess());
-        assertEquals(34, packing.getNetProductivity());
-        assertEquals(35, packing.getTargetProductivity());
-        assertEquals(10, packing.getWorkers().getBusy());
-        assertEquals(10, packing.getWorkers().getIdle());
-        assertEquals(1350, packing.getThroughput());
-
-        assertEquals("fbm-wms-inbound", inbound.getWorkflow());
-        assertEquals(20, inbound.getTotalWorkers());
-        assertEquals(0, inbound.getTotalNonSystemicWorkers());
-        assertEquals(3, inbound.getProcesses().size());
-
-        assertEquals("fbm-wms-withdrawals", withdrawals.getWorkflow());
-        assertEquals(54, withdrawals.getTotalWorkers());
-        assertEquals(24, withdrawals.getTotalNonSystemicWorkers());
-        assertEquals(2, withdrawals.getProcesses().size());
-
         final Process pickingWithdrawals = withdrawals.getProcesses().get(0);
-        assertEquals("picking", pickingWithdrawals.getProcess());
-        assertEquals(45, pickingWithdrawals.getNetProductivity());
-        assertEquals(14, pickingWithdrawals.getWorkers().getBusy());
-        assertEquals(10, pickingWithdrawals.getWorkers().getIdle());
-        assertEquals(2700, pickingWithdrawals.getThroughput());
-
+        final var putAwayAreas = putAway.getAreas();
+        final var pickingAreas = picking.getAreas();
         final var pickingWithdrawalsAreas = pickingWithdrawals.getAreas();
+
+        assertEquals(176, staffing.getTotalWorkers());
+        assertEquals(3, staffing.getWorkflows().size());
+
+        assertEqualsWorkflow(inbound, INBOUND_WORKFLOW, 20,0, 3);
+        assertEqualsProcess(inbound.getProcesses().get(0), RECEIVING_PROCESS, 25, null, 0, 3, 1, 0);
+        assertEqualsProcess(inbound.getProcesses().get(1), CHECK_IN_PROCESS, 35, null, 1, 6, 11, 0);
+        assertEqualsProcess(putAway, PUT_AWAY_PROCESS, 15, null, 1, 9, 21, 2);
+        assertEquals(AREA_MZ1, putAwayAreas.get(0).getArea());
+        assertEquals(0, putAwayAreas.get(0).getWorkers().getIdle());
+        assertEquals(5, putAwayAreas.get(0).getWorkers().getBusy());
+        assertEquals(32, putAwayAreas.get(0).getNetProductivity());
+        assertEquals(AREA_RKL, putAwayAreas.get(1).getArea());
+        assertEquals(1, putAwayAreas.get(1).getWorkers().getIdle());
+        assertEquals(4, putAwayAreas.get(1).getWorkers().getBusy());
+        assertEquals(32, putAwayAreas.get(1).getNetProductivity());
+
+
+        assertEqualsWorkflow(outbound, OUTBOUND_WORKFLOW, 102,20, 5);
+        assertEqualsProcess(picking, PICKING_PROCESS, 45, 30, 6, 20, 2700, 2);
+        assertEquals(AREA_MZ1, pickingAreas.get(0).getArea());
+        assertEquals(4, pickingAreas.get(0).getWorkers().getIdle());
+        assertEquals(10, pickingAreas.get(0).getWorkers().getBusy());
+        assertEquals(60, pickingAreas.get(0).getNetProductivity());
+        assertEquals(AREA_RKL, pickingAreas.get(1).getArea());
+        assertEquals(2, pickingAreas.get(1).getWorkers().getIdle());
+        assertEquals(10, pickingAreas.get(1).getWorkers().getBusy());
+        assertEquals(75, pickingAreas.get(1).getNetProductivity());
+        assertNullProcess(outbound.getProcesses().get(1), BATCH_SORTER_PROCESS);
+        assertEqualsProcess(outbound.getProcesses().get(2), WALL_IN_PROCESS, 0, 20, 0, 0, 0, 0);
+        assertEqualsProcess(outbound.getProcesses().get(3), PACKING_PROCESS, 34, 35, 1, 15, 1350, 0);
+        assertEqualsProcess(outbound.getProcesses().get(4), PACKING_WALL_PROCESS, 32, null, 5, 35, 11, 0);
+
+        assertEqualsWorkflow(withdrawals, WITHDRAWALS_WORKFLOW, 54,24, 2);
+        assertEqualsProcess(pickingWithdrawals, PICKING_PROCESS, 45, null, 10, 14, 2700, 2);
         assertEquals(2, pickingWithdrawalsAreas.size());
-        assertEquals("MZ1", pickingWithdrawalsAreas.get(0).getArea());
-        assertEquals(8, pickingWithdrawalsAreas.get(0).getWorkers().getBusy());
+        assertEquals("MZ-1", pickingWithdrawalsAreas.get(0).getArea());
         assertEquals(6, pickingWithdrawalsAreas.get(0).getWorkers().getIdle());
+        assertEquals(8, pickingWithdrawalsAreas.get(0).getWorkers().getBusy());
         assertEquals(60, pickingWithdrawalsAreas.get(0).getNetProductivity());
-        assertEquals("MZ2", pickingWithdrawalsAreas.get(1).getArea());
-        assertEquals(6, pickingWithdrawalsAreas.get(1).getWorkers().getBusy());
+        assertEquals("RK-L", pickingWithdrawalsAreas.get(1).getArea());
         assertEquals(4, pickingWithdrawalsAreas.get(1).getWorkers().getIdle());
+        assertEquals(6, pickingWithdrawalsAreas.get(1).getWorkers().getBusy());
         assertEquals(75, pickingWithdrawalsAreas.get(1).getNetProductivity());
+        assertEqualsProcess(withdrawals.getProcesses().get(1), PACKING_PROCESS, 34, null, 2, 4, 1350, 0);
     }
 
     @Test
@@ -189,6 +178,40 @@ class GetStaffingTest {
 
         assertNull(withdrawals.getTotalWorkers());
         assertNull(withdrawals.getTotalNonSystemicWorkers());
+    }
+
+    private void assertEqualsWorkflow(final StaffingWorkflow workflow,
+                                              final String name,
+                                              final int totalWorkers,
+                                              final int nsWorkers,
+                                              final int processesSize) {
+
+        assertEquals(name, workflow.getWorkflow());
+        assertEquals(totalWorkers, workflow.getTotalWorkers());
+        assertEquals(nsWorkers, workflow.getTotalNonSystemicWorkers());
+        assertEquals(processesSize, workflow.getProcesses().size());
+    }
+
+    private void assertEqualsProcess(final Process process, final String name, final int netProductivity,
+                                     final Integer targetProductivity, final int idleWorkers, final int busyWorkers,
+                                     final int throughput, final int areasSize) {
+
+        assertEquals(name, process.getProcess());
+        assertEquals(netProductivity, process.getNetProductivity());
+        assertEquals(targetProductivity, process.getTargetProductivity());
+        assertEquals(busyWorkers, process.getWorkers().getBusy());
+        assertEquals(idleWorkers, process.getWorkers().getIdle());
+        assertEquals(throughput, process.getThroughput());
+        assertEquals(areasSize, process.getAreas().size());
+    }
+
+    private void assertNullProcess(final Process process, final String name) {
+        assertEquals(name, process.getProcess());
+        assertNull(process.getNetProductivity());
+        assertNull(process.getTargetProductivity());
+        assertNull(process.getWorkers().getBusy());
+        assertNull(process.getWorkers().getIdle());
+        assertNull(process.getThroughput());
     }
 
     private StaffingResponse mockStaffingResponse() {
@@ -233,19 +256,18 @@ class GetStaffingTest {
         return List.of(
                 new StaffingProcess(
                         "receiving",
-                        new ProcessTotals(15, 30, 25.0, null, 11.10),
+                        new ProcessTotals(0, 3, 25.0, null, 1.10),
                         emptyList()),
                 new StaffingProcess(
                         "check_in",
-                        new ProcessTotals(15, 30, 25.0, null, 11.10),
+                        new ProcessTotals(1, 6, 35.0, null, 11.10),
                         emptyList()),
                 new StaffingProcess(
                         "put_away",
-                        new ProcessTotals(15, 30, 25.0, null, 11.10),
+                        new ProcessTotals(1, 9, 15.0, null, 21.10),
                         List.of(
-                                new Area("MZ-1", new Totals(23, 11, 32.4, 231.3)),
-                                new Area("RK-L", new Totals(23, 11, 32.4, 231.3)),
-                                new Area("RK-H", new Totals(23, 11, 32.4, 231.3))
+                                new Area("MZ-1", new Totals(0, 5, 32.4, 231.3)),
+                                new Area("RK-L", new Totals(1, 4, 32.4, 231.3))
                         ))
         );
     }
@@ -254,10 +276,10 @@ class GetStaffingTest {
         return List.of(
                 new StaffingProcess(
                         "picking",
-                        new ProcessTotals(10, 20, 45.71, null, 2700.0),
+                        new ProcessTotals(6, 20, 45.71, null, 2700.0),
                         List.of(
-                                new Area("MZ1", new Totals(0, 10, 60.33, 3.3)),
-                                new Area("MZ2", new Totals(0, 10, 75.42, 1.3))
+                                new Area("MZ-1", new Totals(4, 10, 60.33, 3.3)),
+                                new Area("RK-L", new Totals(2, 10, 75.42, 1.3))
                         )),
                 new StaffingProcess(
                         "batch_sorter",
@@ -269,11 +291,11 @@ class GetStaffingTest {
                         emptyList()),
                 new StaffingProcess(
                         "packing",
-                        new ProcessTotals(10, 10, null, 34.5, 1350.0),
+                        new ProcessTotals(1, 15, null, 34.5, 1350.0),
                         emptyList()),
                 new StaffingProcess(
                         "packing_wall",
-                        new ProcessTotals(15, 30, null, 32.4, 11.10),
+                        new ProcessTotals(5, 35, null, 32.4, 11.10),
                         emptyList())
         );
     }
@@ -284,8 +306,8 @@ class GetStaffingTest {
                         "picking",
                         new ProcessTotals(10, 14, 45.71, null, 2700.0),
                         List.of(
-                                new Area("MZ1", new Totals(6, 8, 60.33, 3.3)),
-                                new Area("MZ2", new Totals(4, 6, 75.42, 1.3))
+                                new Area("MZ-1", new Totals(6, 8, 60.33, 3.3)),
+                                new Area("RK-L", new Totals(4, 6, 75.42, 1.3))
                         )),
                 new StaffingProcess(
                         "packing",
