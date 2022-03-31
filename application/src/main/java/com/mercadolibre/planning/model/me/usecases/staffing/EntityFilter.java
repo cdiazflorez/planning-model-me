ea59fcea86f1629e2f7911a58461e7efabd546ec
Marcial Manzo <marcial.manzo@mercadolibre.com>
2022-03-31 17:39:28 -0300

    removes Getter annotation from EntityFilter

--- application/src/main/java/com/mercadolibre/planning/model/me/usecases/staffing/EntityFilter.java
+++ application/src/main/java/com/mercadolibre/planning/model/me/usecases/staffing/EntityFilter.java
@@ -11,10 +11,8 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
 
 @AllArgsConstructor
-@Getter
 public enum EntityFilter {
   PRODUCTIVITY_FILTER(
       PRODUCTIVITY,
@@ -24,15 +22,14 @@ public enum EntityFilter {
       HEADCOUNT,
       Map.of(HEADCOUNT, Map.of(PROCESSING_TYPE.toJson(), List.of(ACTIVE_WORKERS.getName()))));
 
-  private MagnitudeType magnitudeType;
+  private final MagnitudeType magnitudeType;
 
   private final Map<MagnitudeType, Map<String, List<String>>> entity;
 
-  static Map<MagnitudeType, Map<String, List<String>>> getEntityFilter(
-      MagnitudeType magnitudeType) {
+  static Map<MagnitudeType, Map<String, List<String>>> getEntityFilter(MagnitudeType magnitudeType) {
     return Arrays.stream(EntityFilter.values())
-        .filter(value -> value.getMagnitudeType().equals(magnitudeType))
-        .map(EntityFilter::getEntity)
+        .filter(value -> value.magnitudeType.equals(magnitudeType))
+        .map(filter -> filter.entity)
         .findAny()
         .get();
   }
