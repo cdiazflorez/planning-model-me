93cef57ce613faa5b789ee6484292e8d7d7eb90a
mercadolibrefalconi <cesar.panta@mercadolibre.com>
2022-05-12 16:08:12 -0300

    Add feature toogle <backlog api> (#556)
    * Add FT call backlogApi
    
    * Add test
    
    * Add resources
    
    * Fix check style
    
    * Add constant check style fix
    
    * remove logic wave suggestion
    
    * Remove spaces
    
    * fix
    
    * Comment resolved

--- application/src/main/java/com/mercadolibre/planning/model/me/gateways/toogle/FeatureSwitches.java
+++ application/src/main/java/com/mercadolibre/planning/model/me/gateways/toogle/FeatureSwitches.java
@@ -2,4 +2,11 @@ package com.mercadolibre.planning.model.me.gateways.toogle;
 
 /** Feature toggles gateway. */
 public interface FeatureSwitches {
+
+    /**
+     * Check enable calls to backlog api.
+     *
+     * @return whether the feature toggle is true.
+     */
+    boolean shouldCallBacklogApi();
 }
