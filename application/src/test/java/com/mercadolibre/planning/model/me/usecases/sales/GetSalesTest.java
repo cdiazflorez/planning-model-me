6367233f14c94d125cf1dda32abf9aeaea42aeb4
mercadolibrefalconi <cesar.panta@mercadolibre.com>
2022-05-20 14:11:01 -0300

    Add call bapi request #1 y #4 (#566)
    * Add FT call backlogApi
    
    * Add test
    
    * Add resources
    
    * Fix check style
    
    * Add constant check style fix
    
    * remove logic wave suggestion
    
    * Remove spaces
    
    * fix
    
    * Add ft request 1 and 4
    
    * Redirect to ba prod
    
    * Changes
    
    * Fix
    
    * Fix test
    
    * Fix test
    
    * Redirect stage
    
    * Redirect to prod ba
    
    * Comment resolved
    
    * Comment resolved
    
    * Fic test
    
    * Fix test

--- application/src/test/java/com/mercadolibre/planning/model/me/usecases/sales/GetSalesTest.java
+++ application/src/test/java/com/mercadolibre/planning/model/me/usecases/sales/GetSalesTest.java
@@ -1,19 +1,5 @@
 package com.mercadolibre.planning.model.me.usecases.sales;
 
-import com.mercadolibre.planning.model.me.entities.projection.Backlog;
-import com.mercadolibre.planning.model.me.gateways.backlog.dto.BacklogFilters;
-import com.mercadolibre.planning.model.me.gateways.outboundunit.UnitSearchGateway;
-import com.mercadolibre.planning.model.me.usecases.sales.dtos.GetSalesInputDto;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
-import java.util.List;
-
 import static com.mercadolibre.planning.model.me.gateways.planningmodel.dtos.Workflow.FBM_WMS_OUTBOUND;
 import static com.mercadolibre.planning.model.me.utils.TestUtils.WAREHOUSE_ID;
 import static java.time.ZoneOffset.UTC;
@@ -21,6 +7,28 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertIterableEquals;
 import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.when;
+
+import com.mercadolibre.planning.model.me.entities.projection.Backlog;
+import com.mercadolibre.planning.model.me.gateways.backlog.BacklogApiGateway;
+import com.mercadolibre.planning.model.me.gateways.backlog.dto.BacklogCurrentRequest;
+import com.mercadolibre.planning.model.me.gateways.backlog.dto.BacklogFilters;
+import com.mercadolibre.planning.model.me.gateways.backlog.dto.Consolidation;
+import com.mercadolibre.planning.model.me.gateways.outboundunit.UnitSearchGateway;
+import com.mercadolibre.planning.model.me.gateways.toogle.FeatureSwitches;
+import com.mercadolibre.planning.model.me.usecases.sales.dtos.GetSalesInputDto;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 public class GetSalesTest {
@@ -31,8 +39,14 @@ public class GetSalesTest {
     @Mock
     private UnitSearchGateway unitSearchGateway;
 
+    @Mock
+    private FeatureSwitches featureSwitches;
+
+    @Mock
+    private BacklogApiGateway backlogApiGateway;
+
     @Test
-    public void testExecuteOk() {
+    public void testExecuteOkCallOutboundUnit() {
         final ZonedDateTime currentTime = ZonedDateTime.now(UTC).withMinute(0).withNano(0);
         final GetSalesInputDto input = GetSalesInputDto.builder()
                 .workflow(FBM_WMS_OUTBOUND)
@@ -42,6 +56,8 @@ public class GetSalesTest {
                 .timeZone(ZoneId.of("America/Bogota"))
                 .fromDS(true)
                 .build();
+
+        when(featureSwitches.shouldCallBacklogApi()).thenReturn(false);
 
         when(unitSearchGateway.getSalesByCpt(any(BacklogFilters.class)))
                 .thenReturn(mockList(currentTime));
@@ -54,7 +70,33 @@ public class GetSalesTest {
         assertIterableEquals(getExpectedSales(currentTime), sales);
     }
 
-    private List<Backlog> mockList(ZonedDateTime currentTime) {
+    @Test
+    public void testExecuteOkCallBacklogApi() {
+        final ZonedDateTime currentTime = ZonedDateTime.now(UTC).withNano(0);
+        final GetSalesInputDto input = GetSalesInputDto.builder()
+                .workflow(FBM_WMS_OUTBOUND)
+                .warehouseId(WAREHOUSE_ID)
+                .dateCreatedFrom(getDateFrom())
+                .dateCreatedTo(currentTime)
+                .dateOutFrom(getDateFrom())
+                .dateOutTo(currentTime)
+                .fromDS(true)
+                .build();
+
+        when(featureSwitches.shouldCallBacklogApi()).thenReturn(true);
+
+        when(backlogApiGateway.getCurrentBacklog(any(BacklogCurrentRequest.class)))
+                .thenReturn(mockListBa(currentTime));
+        // WHEN
+        final List<Backlog> sales = getSales.execute(input);
+
+        // THEN
+        assertEquals(3, sales.size());
+
+        assertIterableEquals(getExpectedSalesBa(currentTime), sales);
+    }
+
+    private List<Backlog> mockList(final ZonedDateTime currentTime) {
         return List.of(
                 new Backlog(currentTime, 2231),
                 new Backlog(currentTime.plusHours(1), 1442),
@@ -62,11 +104,31 @@ public class GetSalesTest {
         );
     }
 
+    private List<Consolidation> mockListBa(final ZonedDateTime currentTime) {
+        return List.of(new Consolidation(currentTime.toInstant(), getRequestKeys(currentTime.toInstant()), 2231, false),
+                new Consolidation(currentTime.toInstant(), getRequestKeys(currentTime.toInstant()), 1442, false),
+                new Consolidation(currentTime.toInstant(), getRequestKeys(currentTime.toInstant()), 725, false));
+    }
+
     private Iterable<?> getExpectedSales(final ZonedDateTime currentTime) {
         return mockList(currentTime);
     }
 
+    private Iterable<?> getExpectedSalesBa(final ZonedDateTime currentTime) {
+        final List<Consolidation> backlogConsolidation = mockListBa(currentTime);
+
+        return backlogConsolidation.stream().map(item -> new Backlog(
+                ZonedDateTime.ofInstant(item.getDate(), ZoneOffset.UTC),
+                item.getTotal())).collect(Collectors.toList());
+    }
+
     private ZonedDateTime getDateFrom() {
         return ZonedDateTime.now(UTC).minusHours(28).withSecond(0).withNano(0);
+    }
+
+    private Map<String, String> getRequestKeys(final Instant cpt) {
+        final Map<String, String> result = new ConcurrentHashMap<>();
+        result.put("date_out", cpt.toString());
+        return result;
     }
 }
