fb6cf22b167b8ae16e6ef7843f59b6f8ab7b411a
fpertuz <fabio.pertuz@mercadolibre.com.co>
2022-08-16 10:22:18 -0500

    :sparkles: [PMME] FWCO-201: Inbound polyvalences added to inbound forecaste template, processed and tested

---
+++ application/src/main/java/com/mercadolibre/planning/model/me/usecases/forecast/parsers/inbound/model/ForecastPolyvalenceInboundProcessName.java
@@ -0,0 +1,23 @@
+package com.mercadolibre.planning.model.me.usecases.forecast.parsers.inbound.model;
+
+import java.util.stream.Stream;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ForecastPolyvalenceInboundProcessName {
+  RECEIVING(2),
+  CHECKIN(3),
+  PUTAWAY(4);
+
+  private final int columnIndex;
+
+  public String getName() {
+    return name().toLowerCase();
+  }
+
+  public static Stream<ForecastPolyvalenceInboundProcessName> stream() {
+    return Stream.of(ForecastPolyvalenceInboundProcessName.values());
+  }
+  }
--- application/src/test/java/com/mercadolibre/planning/model/me/usecases/forecast/parsers/inbound/InboundRepsForecastSheetParserTest.java
+++ application/src/test/java/com/mercadolibre/planning/model/me/usecases/forecast/parsers/inbound/InboundRepsForecastSheetParserTest.java
@@ -16,96 +16,108 @@ import java.util.List;
 import java.util.TimeZone;
 
 import static com.mercadolibre.planning.model.me.usecases.forecast.parsers.inbound.model.ForecastColumnName.HEADCOUNT_PRODUCTIVITY;
+import static com.mercadolibre.planning.model.me.usecases.forecast.parsers.inbound.model.ForecastColumnName.INBOUND_CHECKIN_PRODUCTIVITY_POLYVALENCES;
+import static com.mercadolibre.planning.model.me.usecases.forecast.parsers.inbound.model.ForecastColumnName.INBOUND_PUTAWAY_PRODUCTIVITY_POLIVALENCES;
+import static com.mercadolibre.planning.model.me.usecases.forecast.parsers.inbound.model.ForecastColumnName.INBOUND_RECEIVING_PRODUCTIVITY_POLYVALENCES;
 import static com.mercadolibre.planning.model.me.usecases.forecast.parsers.inbound.model.ForecastColumnName.PROCESSING_DISTRIBUTION;
 import static com.mercadolibre.planning.model.me.utils.TestUtils.getMeliSheetFrom;
 import static java.time.format.DateTimeFormatter.ISO_OFFSET_DATE_TIME;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ExtendWith(MockitoExtension.class)
 class InboundRepsForecastSheetParserTest {
 
-    private static final String VALID_FILE_PATH = "inbound_planning_ok.xlsx";
-    private static final String ERRONEOUS_FILE_PATH = "inbound_planning_with_errors.xlsx";
-    private static final LogisticCenterConfiguration CONF =
-            new LogisticCenterConfiguration(TimeZone.getTimeZone("UTC"));
-    private static final ZonedDateTime FIRST_DATE = ZonedDateTime.parse("2021-11-07T00:00Z", ISO_OFFSET_DATE_TIME);
+  private static final String VALID_FILE_PATH = "inbound_planning_ok.xlsx";
 
-    private final InboundRepsForecastSheetParser parser = new InboundRepsForecastSheetParser();
+  private static final String ERRONEOUS_FILE_PATH = "inbound_planning_with_errors.xlsx";
 
-    @Test
-    @DisplayName("Excel Parsed Ok")
-    void parseOk() {
-        // GIVEN
-        final MeliSheet repsSheet = getMeliSheetFrom(parser.name(), VALID_FILE_PATH);
+  private static final LogisticCenterConfiguration CONF =
+      new LogisticCenterConfiguration(TimeZone.getTimeZone("UTC"));
 
-        // WHEN
-        final ForecastSheetDto result = parser.parse("ARBA01", repsSheet, CONF);
+  private static final ZonedDateTime FIRST_DATE = ZonedDateTime.parse("2021-11-07T00:00Z", ISO_OFFSET_DATE_TIME);
 
-        // THEN
-        assertNotNull(result);
-        assertEquals("Plan de staffing", result.getSheetName());
-        assertEquals(4, result.getValues().size());
+  private final InboundRepsForecastSheetParser parser = new InboundRepsForecastSheetParser();
 
-        // PROCESSING DISTRIBUTIONS
-        final var processingDistributions = (List<ProcessingDistribution>) result.getValues()
-                .get(PROCESSING_DISTRIBUTION);
+  @Test
+  @DisplayName("Excel Parsed Ok")
+  void parseOk() {
+    // GIVEN
+    final MeliSheet repsSheet = getMeliSheetFrom(parser.name(), VALID_FILE_PATH);
 
-        assertNotNull(processingDistributions);
-        assertEquals(17, processingDistributions.size());
+    // WHEN
+    final ForecastSheetDto result = parser.parse("ARBA01", repsSheet, CONF);
 
-        final var receivingTarget = processingDistributions.get(0);
-        assertEquals("receiving", receivingTarget.getProcessName());
-        assertEquals(168, receivingTarget.getData().size());
+    // THEN
+    assertNotNull(result);
+    assertEquals("Plan de staffing", result.getSheetName());
+    assertEquals(7, result.getValues().size());
 
-        final var firstRow = receivingTarget.getData().get(0);
-        assertEquals(FIRST_DATE, firstRow.getDate());
-        assertEquals(0, firstRow.getQuantity());
+    // PROCESSING DISTRIBUTIONS
+    final var processingDistributions = (List<ProcessingDistribution>) result.getValues()
+        .get(PROCESSING_DISTRIBUTION);
 
-        // PRODUCTIVITY
-        final var productivities = (List<HeadcountProductivity>) result.getValues()
-                .get(HEADCOUNT_PRODUCTIVITY);
+    assertNotNull(processingDistributions);
+    assertEquals(17, processingDistributions.size());
 
-        assertNotNull(productivities);
-        assertEquals(2, productivities.size());
+    final var receivingTarget = processingDistributions.get(0);
+    assertEquals("receiving", receivingTarget.getProcessName());
+    assertEquals(168, receivingTarget.getData().size());
 
-        final var checkInProductivities = productivities.get(0);
-        assertEquals("check_in", checkInProductivities.getProcessName());
-        assertEquals(168, checkInProductivities.getData().size());
+    final var firstRow = receivingTarget.getData().get(0);
+    assertEquals(FIRST_DATE, firstRow.getDate());
+    assertEquals(0, firstRow.getQuantity());
 
-        final var checkInProductivity = checkInProductivities.getData().get(0);
-        assertEquals(FIRST_DATE, checkInProductivity.getDayTime());
-        assertEquals(407, checkInProductivity.getProductivity());
+    // PRODUCTIVITY
+    final var productivities = (List<HeadcountProductivity>) result.getValues()
+        .get(HEADCOUNT_PRODUCTIVITY);
 
-        final var putAwayProductivities = productivities.get(1);
-        assertEquals("put_away", putAwayProductivities.getProcessName());
-        assertEquals(168, putAwayProductivities.getData().size());
+    assertNotNull(productivities);
+    assertEquals(2, productivities.size());
 
-        final var putAwayProductivity = putAwayProductivities.getData().get(0);
-        assertEquals(FIRST_DATE, putAwayProductivity.getDayTime());
-        assertEquals(313, putAwayProductivity.getProductivity());
-    }
+    final var checkInProductivities = productivities.get(0);
+    assertEquals("check_in", checkInProductivities.getProcessName());
+    assertEquals(168, checkInProductivities.getData().size());
 
-    @Test
-    @DisplayName("Excel With Errors")
-    void errors() {
-        // GIVEN
-        final MeliSheet repsSheet = getMeliSheetFrom(parser.name(), ERRONEOUS_FILE_PATH);
+    final var checkInProductivity = checkInProductivities.getData().get(0);
+    assertEquals(FIRST_DATE, checkInProductivity.getDayTime());
+    assertEquals(407, checkInProductivity.getProductivity());
 
-        // WHEN
-        final ForecastParsingException exception = assertThrows(ForecastParsingException.class,
-                () -> parser.parse("ARBA01", repsSheet, CONF));
+    final var putAwayProductivities = productivities.get(1);
+    assertEquals("put_away", putAwayProductivities.getProcessName());
+    assertEquals(168, putAwayProductivities.getData().size());
 
-        // THEN
-        assertNotNull(exception.getMessage());
+    final var putAwayProductivity = putAwayProductivities.getData().get(0);
+    assertEquals(FIRST_DATE, putAwayProductivity.getDayTime());
+    assertEquals(313, putAwayProductivity.getProductivity());
 
-        final String expectedMessage =
-                "Error while trying to parse cell (D15) for sheet: Plan de staffing.\n"
-                + "Error while trying to parse cell (B27) for sheet: Plan de staffing.\n"
-                + "Error while trying to parse cell (J32) for sheet: Plan de staffing";
+    // POLYVALENCES
+    assertTrue(result.getValues().containsKey(INBOUND_CHECKIN_PRODUCTIVITY_POLYVALENCES));
+    assertTrue(result.getValues().containsKey(INBOUND_PUTAWAY_PRODUCTIVITY_POLIVALENCES));
+    assertTrue(result.getValues().containsKey(INBOUND_RECEIVING_PRODUCTIVITY_POLYVALENCES));
+  }
 
-        assertEquals(expectedMessage, exception.getMessage());
-    }
+  @Test
+  @DisplayName("Excel With Errors")
+  void errors() {
+    // GIVEN
+    final MeliSheet repsSheet = getMeliSheetFrom(parser.name(), ERRONEOUS_FILE_PATH);
+
+    // WHEN
+    final ForecastParsingException exception = assertThrows(ForecastParsingException.class,
+        () -> parser.parse("ARBA01", repsSheet, CONF));
+
+    // THEN
+    assertNotNull(exception.getMessage());
+
+    final String expectedMessage =
+        "Error while trying to parse cell (D15) for sheet: Plan de staffing.\n"
+            + "Error while trying to parse cell (B27) for sheet: Plan de staffing.\n"
+            + "Error while trying to parse cell (J32) for sheet: Plan de staffing";
+
+    assertEquals(expectedMessage, exception.getMessage());
+  }
 
 }
