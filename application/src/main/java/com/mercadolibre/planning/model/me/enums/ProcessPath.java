3474c03d395c0484fa9dc382764e65768db84433
mercadolibrefalconi <cesar.panta@mercadolibre.com>
2022-11-08 15:11:51 -0300

    Add PP Parsed (#745)
    * Add PP Parsed
    
    * Code quality resolved
    
    * Comment resolved
    
    * Compatibilidad hacia atras
    
    * Code quality resolved
    
    * Remove warehouse

--- application/src/main/java/com/mercadolibre/planning/model/me/enums/ProcessPath.java
+++ application/src/main/java/com/mercadolibre/planning/model/me/enums/ProcessPath.java
@@ -2,21 +2,34 @@ package com.mercadolibre.planning.model.me.enums;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
+import java.util.Arrays;
 import java.util.Locale;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+@Getter
+@AllArgsConstructor
 public enum ProcessPath {
-  GLOBAL,
-  TOT_MONO,
-  TOT_MULTI_BATCH,
-  TOT_MULTI_ORDER,
-  NON_TOT_MONO,
-  NON_TOT_MULTI_ORDER,
-  NON_TOT_MULTI_BATCH,
-  PP_DEFAULT_MONO,
-  PP_DEFAULT_MULTI,
-  SIOC,
-  AMBIENT,
-  REFRIGERATED;
+  GLOBAL("global"),
+  TOT_MONO("totable mono"),
+  TOT_MULTI_BATCH("totable multi batch"),
+  TOT_MULTI_ORDER("totable multi order"),
+  NON_TOT_MONO("non totable mono"),
+  NON_TOT_MULTI_ORDER("non tot multi order"),
+  NON_TOT_MULTI_BATCH("non tot multi batch"),
+  PP_DEFAULT_MONO("pp default mono"),
+  PP_DEFAULT_MULTI("pp default multi"),
+  SIOC("sioc"),
+  AMBIENT("ambient"),
+  REFRIGERATED("refrigerated");
+
+  private static final Map<String, ProcessPath> PROCESS_PATH_BY_DESCRIPTION = Arrays.stream(ProcessPath.values())
+      .collect(Collectors.toMap(ProcessPath::getDescription, Function.identity()));
+
+  private final String description;
 
   @JsonCreator
   public static ProcessPath from(final String value) {
@@ -26,5 +39,9 @@ public enum ProcessPath {
   @JsonValue
   public String getName() {
     return name().toLowerCase(Locale.getDefault());
+  }
+
+  public static ProcessPath fromDescription(final String description) {
+    return PROCESS_PATH_BY_DESCRIPTION.get(description.toLowerCase(Locale.getDefault()));
   }
 }
---
+++ application/src/main/java/com/mercadolibre/planning/model/me/usecases/forecast/parsers/outbound/model/ForecastProcessPathConfiguration.java
@@ -0,0 +1,39 @@
+package com.mercadolibre.planning.model.me.usecases.forecast.parsers.outbound.model;
+
+import java.util.List;
+import java.util.Locale;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ForecastProcessPathConfiguration {
+  PD_DATE_OUT_COLUMN(1, 1),
+  PD_DATE_IN_COLUMN(2, 2),
+  PROCESS_PATH_ID_COLUMN(null, 3),
+  CARRIER_ID_COLUMN(3, 4),
+  SERVICE_ID_COLUMN(4, 5),
+  CANALIZATION_COLUMN(5, 6),
+  PD_QUANTITY_COLUMN(6, 7);
+
+  //TODO Add BRBA01 when starting pilot.
+  private static final List<String> LOGISTIC_CENTER_WITH_PP = List.of();
+
+  private static final int COLUMN_COUNT = values().length;
+
+  private final Integer offPP;
+
+  private final Integer onPP;
+
+  public static boolean hasProcessPath(final String logisticCenter) {
+    return LOGISTIC_CENTER_WITH_PP.contains(logisticCenter.toUpperCase(Locale.getDefault()));
+  }
+
+  public static int maxValidateColumnIndex(final String logisticCenter) {
+    return hasProcessPath(logisticCenter) ? COLUMN_COUNT : COLUMN_COUNT - 1;
+  }
+
+  public Integer getPosition(final String logisticCenter) {
+    return hasProcessPath(logisticCenter) ? this.onPP : this.offPP;
+  }
+}
