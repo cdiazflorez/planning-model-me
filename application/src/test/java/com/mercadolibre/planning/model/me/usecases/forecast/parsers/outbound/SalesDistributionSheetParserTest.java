3e7b794c1d7b0e515456a31401974d89f7a1b361
Cesar Antonio Falconi <cesar.panta@mercadolibre.com>
2022-11-09 18:15:43 -0300

    Comment resolved

--- application/src/test/java/com/mercadolibre/planning/model/me/usecases/forecast/parsers/outbound/SalesDistributionSheetParserTest.java
+++ application/src/test/java/com/mercadolibre/planning/model/me/usecases/forecast/parsers/outbound/SalesDistributionSheetParserTest.java
@@ -3,14 +3,21 @@ package com.mercadolibre.planning.model.me.usecases.forecast.parsers.outbound;
 import static com.mercadolibre.planning.model.me.usecases.forecast.parsers.outbound.model.ForecastSheet.ORDER_DISTRIBUTION;
 import static com.mercadolibre.planning.model.me.utils.TestUtils.WAREHOUSE_ID;
 import static com.mercadolibre.planning.model.me.utils.TestUtils.getMeliSheetFrom;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import com.mercadolibre.planning.model.me.enums.ProcessPath;
 import com.mercadolibre.planning.model.me.exception.ForecastParsingException;
 import com.mercadolibre.planning.model.me.gateways.logisticcenter.dtos.LogisticCenterConfiguration;
+import com.mercadolibre.planning.model.me.gateways.planningmodel.dtos.PlanningDistribution;
 import com.mercadolibre.planning.model.me.usecases.forecast.dto.ForecastSheetDto;
 import com.mercadolibre.spreadsheet.MeliSheet;
+import java.time.ZonedDateTime;
+import java.util.List;
 import java.util.TimeZone;
+import java.util.stream.Collectors;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -55,8 +62,20 @@ public class SalesDistributionSheetParserTest {
     // WHEN
     final ForecastSheetDto forecastSheetDto = salesDistributionSheetParser.parse(WAREHOUSE_ID, repsSheet, CONF);
 
+    var rows = forecastSheetDto.getValues().values().iterator().next();
+    @SuppressWarnings("unchecked") var firstRow = ((List<PlanningDistribution>)rows).get(0);
+
     // THEN
     assertNotNull(forecastSheetDto);
+    assertEquals(firstRow.getDateIn(), ZonedDateTime.parse("2022-10-31T03:00Z"));
+    assertEquals(firstRow.getDateOut(), ZonedDateTime.parse("2022-10-31T07:00Z"));
+    assertEquals(firstRow.getCarrierId(), "webpack");
+    assertEquals(firstRow.getServiceId(), "0");
+    assertEquals(firstRow.getCanalization(), "web202");
+    assertEquals(firstRow.getProcessPath(), ProcessPath.NON_TOT_MONO);
+    assertEquals(firstRow.getQuantity(), 237.0);
+    assertEquals(firstRow.getQuantityMetricUnit(), "units");
+    assertEquals(firstRow.getMetadata(), List.of());
   }
 
   @Test
