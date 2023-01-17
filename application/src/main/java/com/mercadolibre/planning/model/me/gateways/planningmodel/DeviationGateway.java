2295a02eef2f8e0c6be72b3771b1654c95218521
Marcial Manzo <marcial.manzo@mercadolibre.com>
2023-01-17 15:14:10 -0300

    Disable Inbound Deviations (#837)
    Agrega endpoint para desactivar las desviaciones reutilizando la lógica existente.

--- application/src/main/java/com/mercadolibre/planning/model/me/gateways/planningmodel/DeviationGateway.java
+++ application/src/main/java/com/mercadolibre/planning/model/me/gateways/planningmodel/DeviationGateway.java
@@ -1,12 +1,16 @@
 package com.mercadolibre.planning.model.me.gateways.planningmodel;
 
 import com.mercadolibre.planning.model.me.gateways.planningmodel.dtos.Deviation;
+import com.mercadolibre.planning.model.me.gateways.planningmodel.dtos.DeviationResponse;
 import com.mercadolibre.planning.model.me.gateways.planningmodel.dtos.Workflow;
+import com.mercadolibre.planning.model.me.usecases.deviation.dtos.DisableDeviationInput;
 import java.time.ZonedDateTime;
 import java.util.List;
 
 public interface DeviationGateway {
 
-    List<Deviation> getActiveDeviations(Workflow workflow, String warehouseId, ZonedDateTime date);
+  List<Deviation> getActiveDeviations(Workflow workflow, String warehouseId, ZonedDateTime date);
+
+  DeviationResponse disableDeviation(DisableDeviationInput saveDeviationInput);
 
 }
