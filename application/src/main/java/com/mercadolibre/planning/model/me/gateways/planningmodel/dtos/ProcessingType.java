2715cf2da9c356b3bfeb2fc9bf6f9ed32b06733f
Felipe Bautista <felipe.bautista@mercadolibre.com.co>
2023-01-24 17:22:11 -0500

    fix processing type when type is null

--- application/src/main/java/com/mercadolibre/planning/model/me/gateways/planningmodel/dtos/ProcessingType.java
+++ application/src/main/java/com/mercadolibre/planning/model/me/gateways/planningmodel/dtos/ProcessingType.java
@@ -2,13 +2,13 @@ package com.mercadolibre.planning.model.me.gateways.planningmodel.dtos;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
+import java.util.Arrays;
 import lombok.Getter;
 
 @Getter
 public enum ProcessingType {
     ACTIVE_WORKERS,
     ACTIVE_WORKERS_NS,
-    TOTAL_WORKERS_NS,
     PERFORMED_PROCESSING,
     REMAINING_PROCESSING,
     WORKERS,
@@ -21,7 +21,10 @@ public enum ProcessingType {
 
     @JsonCreator
     public static ProcessingType from(final String value) {
-        return valueOf(value.toUpperCase());
+        return Arrays.stream(values())
+                .filter(v -> v.getName().equals(value))
+                .findFirst()
+                .orElse(null);
     }
 
     @JsonValue
